d8e80d327aa7080f6cd77f9020322aa774de5ddb
WintonPHz <forgotenight@gamil.com>
2023-12-18 19:16:58 -0700

    day 17

--- 2023/Day17/d17.py
+++ 2023/Day17/d17.py
@@ -1,63 +1,57 @@
-import math
-from copy import deepcopy
-import copy
 import heapq
-from functools import cache
 
 class d17():
     def __init__(self, hash_map, max_x, max_y):
         self.map = hash_map
         self.max_x = max_x
         self.max_y = max_y
-        self.min_path = []
-        self.best_goal = max_x*100+max_y*100
-        #self.goal = [max_x, max_y]
-        self.goal = [0,0]
+        self.best_goal = max_x*10+max_y*10
+        self.goal = [max_x, max_y]
+        self.max_move = 3
+        #self.goal = [0,0]
 
-    def look_around(self, position):
+    def look_around(self, position, low, high):
         around_me = [[1, 0, 0], [0, 1, 1], [-1, 0, 2], [0, -1, 3]]
         to_look = []
         for dx, dy, direction in around_me:
-            nx = position[0] + dx
-            ny = position[1] + dy
-
-            if 0 <= nx <= self.max_x and 0 <= ny <= self.max_y:
-                to_look.append([nx, ny, direction])
+            for i in range(low, high + 1):
+                nx = position[0] + dx*i
+                ny = position[1] + dy*i
+                if 0 <= nx <= self.max_x and 0 <= ny <= self.max_y:
+                    to_look.append([nx, ny, direction, i])
         return to_look
 
-    def man_dist_end(self, location):
-        return abs(location[0] - self.goal[0]) + abs(location[1] - self.goal[1])
-
-    def dfs(self, current_location, cur_heat, past_steps, direction = -1, count = 0):
-        #print(current_location, cur_heat, past_steps)
-        past_steps = deepcopy(past_steps)
-        x, y = current_location
-        past_steps.append([x, y])
-        if current_location == self.goal:
-            if cur_heat < self.best_goal:
-                print("Got", cur_heat, "old", self.best_goal)
-                self.best_goal = cur_heat
-            self.print_path(past_steps)
-            return
-        cur_heat += self.map[y][x]
-        where_to_look = self.look_around([x, y])
-        next_moves = []
-        for nx, ny, next_direction in where_to_look:
-            cur_count = count
-            if [nx, ny] in past_steps:
+    def dijk(self, low, high):
+        avaialable_moves = []
+        heapq.heappush(avaialable_moves, (0, [0,0], -50))
+        visited = set()
+        while True:
+            cur_heat, best_move, direction = heapq.heappop(avaialable_moves)
+            x, y = best_move
+            if [x, y] == self.goal:
+                return cur_heat
+            if (tuple(best_move + [direction])) in visited:
                 continue
-            if next_direction == direction:
-                next_count = cur_count + 1
-            else:
-                next_count = 0
-            if next_count > 2:
-                continue
-            next_heat = self.map[ny][nx]
-            heapq.heappush(next_moves, (self.man_dist_end([nx, ny]) + cur_heat + next_heat, [nx, ny], next_direction, next_count))
-        while next_moves:
-            check, nm, next_direction, next_count = heapq.heappop(next_moves)
-            #print(next_moves)
-            self.dfs(nm, cur_heat, deepcopy(past_steps), next_direction, next_count)
+            visited.add(tuple(best_move + [direction]))
+            where_to_look = self.look_around([x,y], low, high)
+            next_moves = []
+            for nx, ny, ndir, move_dist in where_to_look:
+                if abs(direction - ndir) == 2 or ndir == direction:
+                    # Dont go the way you came, or the way you were going
+                    continue
+                nh = cur_heat
+                for i in range(1, move_dist+1):
+                    if ndir == 0:
+                        nh += self.map[y][x+i]
+                    elif ndir == 1:
+                        nh += self.map[y+i][x]
+                    elif ndir == 2:
+                        nh += self.map[y][x-i]
+                    elif ndir == 3:
+                        nh += self.map[y-i][x]
+                next_moves.append([[nx, ny], ndir, nh])
+            for nm, ndir, next_heat in next_moves:
+                heapq.heappush(avaialable_moves, (next_heat, nm, ndir))
 
     def print_path(self, path):
         for j, row in self.map.items():
@@ -79,6 +73,7 @@ with open("input.txt", "r") as file:
         puz_in[j] = to_append
 
 obj = d17(puz_in, i, j)
-obj.dfs([i,j], 0, [])
 
-print("Answer 1:", obj.best_goal)
+
+print("Answer 1:", obj.dijk(1, 3))
+print("Answer 2:", obj.dijk(4, 10))
